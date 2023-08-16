7b1c73931a607fbe6752c8b8843f73d1d8c8d4c5
ptrv-dev <petrov99119@gmail.com>
2023-08-16 20:57:49 +0300

    chat: model updated

--- src/chat/models/chat.model.ts
+++ src/chat/models/chat.model.ts
@@ -1,6 +1,13 @@
-import { Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  JoinTable,
+  ManyToMany,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 import { User } from 'src/user/models/user.model';
+import { Message } from 'src/message/models/message.model';
 
 @Entity({ name: 'chats' })
 export class Chat {
@@ -10,4 +17,7 @@ export class Chat {
   @ManyToMany(() => User)
   @JoinTable({ name: 'users_chats' })
   users: User[];
+
+  @OneToMany(() => Message, (message) => message.chat)
+  messages: Message[];
 }
