d21108ef53d6f78c82c42f844f70d5f208094c8a
ptrv-dev <petrov99119@gmail.com>
2023-08-16 13:40:26 +0300

    user: user model

---
+++ src/user/models/user.model.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  username: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  password: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  email: string;
+}
