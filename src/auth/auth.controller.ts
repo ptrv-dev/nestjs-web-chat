470b844634c7d0b984e16ac30ef026a99ccca0b1
ptrv-dev <petrov99119@gmail.com>
2023-08-16 15:23:30 +0300

    auth: test auth route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+
 import { AuthService } from './auth.service';
 import { UserCreateDto } from 'src/user/dto/user-create.dto';
 import { SignInDto } from './dto/sign-in.dto';
+import { AuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -14,8 +23,18 @@ export class AuthController {
   }
 
   @Post('sign-in')
+  @HttpCode(200)
   async signIn(@Body() dto: SignInDto) {
     const token = await this.authService.signIn(dto);
     return { token };
+  }
+
+  @Get('test')
+  @HttpCode(200)
+  @UseGuards(AuthGuard)
+  async test() {
+    return {
+      message: 'You are logged in!',
+    };
   }
 }
